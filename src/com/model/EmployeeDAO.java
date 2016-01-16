9e602487840f565e4072bc3fb3af9aa7ad6825f5
tpahare <tpahare@andrew.cmu.edu>
2016-01-16 11:40:45 -0500

    Added the changePassword() method for the employee

--- src/com/model/EmployeeDAO.java
+++ src/com/model/EmployeeDAO.java
@@ -3,14 +3,31 @@ package com.model;
 import org.genericdao.ConnectionPool;
 import org.genericdao.DAOException;
 import org.genericdao.GenericDAO;
+import org.genericdao.MatchArg;
+import org.genericdao.RollbackException;
+import org.genericdao.Transaction;
 
 import com.databean.EmployeeBean;
-
-
 
 public class EmployeeDAO extends GenericDAO<EmployeeBean> {
 	public EmployeeDAO(ConnectionPool cp, String tableName) throws DAOException {
 		super(EmployeeBean.class, tableName, cp);
 	}
-    
+
+	public void changePassword(String username, String password) throws RollbackException {
+		try {
+			Transaction.begin();
+			EmployeeBean[] employee = match(MatchArg.and(MatchArg.equals("username", username)),
+					MatchArg.equals("password", password));
+			if (employee.length == 0) {
+				throw new RollbackException("Username and password don't match");
+			}
+			employee[0].setPassword(password);
+			update(employee[0]);
+			Transaction.commit();
+		} finally {
+			if (Transaction.isActive())
+				Transaction.rollback();
+		}
+	}
 }

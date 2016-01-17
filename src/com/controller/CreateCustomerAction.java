1d86f1f17fb60367f3650f789038dccd95a83533
Qinjie Guo <qinjieg@andrew.cmu.edu>
2016-01-17 15:34:52 -0500

    createEmployee

---
+++ src/com/controller/CreateCustomerAction.java
@@ -0,0 +1,19 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+
+
+import org.genericdao.MatchArg;
+import org.genericdao.RollbackException;
+import org.mybeans.form.FormBeanException;
+import org.mybeans.form.FormBeanFactory;
+
+public class CreateCustomerAction {
+	
+}

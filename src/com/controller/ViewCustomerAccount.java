c72e8f598fbe0011ed387877ecee3ea9391ff296
ShahnewazFaisal <sfaisal@andrew.cmu.edu>
2016-01-20 14:02:56 -0500

    fff

--- src/com/controller/ViewCustomerAccount.java
+++ src/com/controller/ViewCustomerAccount.java
@@ -5,12 +5,16 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.genericdao.RollbackException;
+
+import com.databean.CustomerBean;
 import com.model.CustomerDAO;
 import com.model.Model;
 
 
 public class ViewCustomerAccount extends Action{
 
+	
 	private CustomerDAO cDAO;
 	
 	public ViewCustomerAccount(Model model) {
@@ -28,7 +32,24 @@ public class ViewCustomerAccount extends Action{
 		List<String> errors = new ArrayList<String>();
 		request.setAttribute("errors",errors);
 		
+		System.out.println("a");
+		
 		//1.Check if any customer account? 2. get them in a list 3.reload to bean each one and  
+		
+		CustomerBean[] customerList;
+		try {
+			customerList = cDAO.getCustomerList();
+			
+			for(CustomerBean list: customerList) {
+				System.out.println(list);
+			}
+			
+		} catch (RollbackException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
 		
 		return null;
 	}

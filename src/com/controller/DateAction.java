62e73c78d0c4207b89319434279d71bfa6d515ce
Qinjie Guo <qinjieg@andrew.cmu.edu>
2016-01-24 14:21:17 -0500

    c

---
+++ src/com/controller/DateAction.java
@@ -0,0 +1,94 @@
+package com.controller;
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.ParseException;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.genericdao.RollbackException;
+import org.mybeans.form.FormBeanException;
+import org.mybeans.form.FormBeanFactory;
+
+import com.databean.FundBean;
+import com.databean.FundPriceHistoryBean;
+import com.form.DateForm;
+import com.model.FundDAO;
+import com.model.FundPriceHistoryDAO;
+import com.model.Model;
+
+public class DateAction extends Action {
+	private FormBeanFactory<DateForm> formBeanFactory = FormBeanFactory.getInstance(DateForm.class);
+	private FundPriceHistoryDAO fundPriceHistoryDAO;
+	private FundDAO fundDAO;
+
+	public DateAction(Model model) {
+		fundPriceHistoryDAO = model.getFundPriceHistoryDAO();
+		fundDAO = model.getFundDAO();
+	}
+
+	@Override
+	public String getName() {
+		return "Date.do";
+	}
+
+	@Override
+	public String perform(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		if(session == null){
+			return "Index.jsp";
+		}
+		List<String> errors = new ArrayList<String>();
+		request.setAttribute("errors", errors);
+		
+		try{
+			DateForm form = formBeanFactory.create(request);
+			request.setAttribute("form", form);
+			if(!form.isPresent()){
+				return "TransitionDay.jsp";
+			}
+			
+			errors.addAll(form.getValidationErrors());
+			
+			if(errors.size() > 0){
+				return "TransitionDay.jsp";
+			}
+		
+			System.out.println("DATE: "+ form.getPricedate());
+			SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+			SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd");
+			
+				String date = fundPriceHistoryDAO.getMaxDate();
+				Date maxdate = sdf.parse(date);
+				
+				Date transactionDate = sdf1.parse(form.getPricedate());
+				if(transactionDate.compareTo(maxdate) <= 0){
+					errors.add("Transition day for this date has already occured");
+					return "TransitionDay.jsp";
+				}
+				session.setAttribute("date", sdf.format(transactionDate));
+			
+			FundBean[] fundBeans = fundDAO.match();
+			request.setAttribute("fundBeans", fundBeans);
+			
+			return "TransitionDayInput.jsp";
+		}catch (FormBeanException e) {
+			errors.add(e.toString());
+			return "TransitionDay.jsp";
+		} catch (RollbackException e1) {
+			errors.add(e1.toString());
+			return "TransitionDay.jsp";
+		} catch (ParseException e) {
+			errors.add("Parse Exception");
+			return "TransitionDay.jsp";
+		}
+
+	}
+
+}
